ec02c4e1a70b1be1334a96b0ae03001b72539792
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2019-11-06 08:30:21 -0800

    Added directory and views files

--- server.js
+++ server.js
@@ -29,10 +29,8 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 //___________________
 //use public folder for static assets
 app.use(express.static('public'));
-
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //use method override
 app.use(methodOverride('_method')); //allow POST, PUT and DELETE from a form
@@ -42,8 +40,14 @@ app.use(methodOverride('_method')); //allow POST, PUT and DELETE from a form
 //___________________
 //localhost:3000
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index.ejs')
 });
+
+//create path to mongodb
+
+mongoose.connect('mongodb://localhost:27017/bashboard', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
+  console.log('The connection with mongod is established')
+})
 
 //___________________
 //Listener
